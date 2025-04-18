aa5d2651fbe9f4414d5821df08276d0fe3eba7e1
christian calderon <print.christiancalderon@gmail.com>
2025-04-18 05:59:44 -0700

    Finished the valut

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = (100 - 20) / 2;
+
+let code3 = 79 % 40;
+
+let message = " the vault has been secured. The combination is ";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
